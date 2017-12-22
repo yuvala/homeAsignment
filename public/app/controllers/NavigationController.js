abe9fe267cec88ae08639c5a6929c46daf0300b0
yuval almaliah <yuv.almaliah@hmail.com>
2017-12-23 00:06:59 +0200

    adding changes

--- public/app/controllers/NavigationController.js
+++ public/app/controllers/NavigationController.js
@@ -1,22 +1,31 @@
-app.controller('NavCtrl', ['$scope', 'FavoritesSvc',
-    function ($scope, FavoritesSvc) {
+app.controller('NavCtrl', ['$scope', '$state', 'FavoritesSvc',
+    function ($scope, $state, FavoritesSvc) {
 
         var vm = this;
         vm.totalFavNum = 0;
-        selectedTab = 1;
-        vm.isSelected = function name(currentSelected) {
+        
+        selectedTab = 'favorites';
+        vm.isSelected = function (currentSelected) {
             return selectedTab === currentSelected;
         };
-        vm.setSelected = function name(currentSelected) {
-            return selectedTab = currentSelected;
+         
+        setSelected = function (currentSelected) {
+             return selectedTab = currentSelected;
+        };
+ 
+        vm.navTo = function (sref) {
+            $state.go(sref);
+            setSelected(sref)
         };
 
         function init() {
             FavoritesSvc.asignToTotalFavNumChanges(setTotalFavNum);
         }
+
         function setTotalFavNum(num) {
             vm.totalFavNum = num;
         }
+        
         init();
     }]
 );
